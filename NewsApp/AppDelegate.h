75c15ecc59426ef23a28be01087e7e94ce85c470
Prabh Dhaliwal <prabh@Prabhs-MacBook-Pro.local>
2014-09-20 13:12:36 +1000

    Initial Commit

---
+++ NewsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsApp
+//
+//  Created by Prabh Dhaliwal on 9/20/14.
+//  Copyright (c) 2014 WestPac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsApp
+//
+//  Created by Prabh Dhaliwal on 9/20/14.
+//  Copyright (c) 2014 WestPac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
